4d7f9a734e423107af34792ba2fc8998c68a57a8
Yasen Simeonov <yasen@brillsolution.com>
2018-01-08 23:41:48 +0200

    Initial commit

---
+++ TestMakerFreeWebApp/ClientApp/app/app.shared.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './components/app/app.component';
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
+import { QuizListComponent } from './components/quiz/quiz-list.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        HomeComponent,
+        QuizListComponent        
+    ],
+    imports: [
+        CommonModule,
+        HttpClientModule,
+        FormsModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: '**', redirectTo: 'home' }
+        ])
+    ]
+})
+
+export class AppModuleShared {
+}
---
+++ TestMakerFreeWebApp/ClientApp/app/components/quiz/quiz-list.component.ts
@@ -0,0 +1,30 @@
+﻿import { Component, Inject} from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+    selector: "quiz-list",
+    templateUrl: './quiz-list.component.html',
+    styleUrls: ['./quiz-list.component.css']
+})
+
+export class QuizListComponent {
+    title: string;
+    selectedQuiz: Quiz;
+    quizzes: Quiz[];
+
+    constructor(private http: HttpClient,
+        @Inject('BASE_URL') baseUrl: string) {
+        this.title = "Latest Quizzes";
+        var url = baseUrl + "api/quiz/Latest/";
+        this.http.get<Quiz[]>(url).subscribe(result => {
+            this.quizzes = result;
+        }, error => console.error(error));
+    }
+
+    onSelect(quiz: Quiz) {
+        this.selectedQuiz = quiz;
+        console.log("quiz with Id "
+            + this.selectedQuiz.Id
+            + " has been selected.");
+    }
+}
