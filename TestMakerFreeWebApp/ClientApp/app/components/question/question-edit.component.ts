d02e56728feffe8c36e67f73a954c506779f9768
Yasen Simeonov <yasen@brillsolution.com>
2018-02-19 09:10:31 +0200

    Added quiz-edit and question-edit component validators.

--- TestMakerFreeWebApp/ClientApp/app/components/question/question-edit.component.ts
+++ TestMakerFreeWebApp/ClientApp/app/components/question/question-edit.component.ts
@@ -1,4 +1,5 @@
 ﻿import { Component, Inject, OnInit } from "@angular/core";
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
 
@@ -11,6 +12,7 @@ import { HttpClient } from "@angular/common/http";
 export class QuestionEditComponent {
     title: string;
     question: Question;
+    form: FormGroup;
 
     // this will be TRUE when editing an existing question,
     // FALSE when creating a new one.
@@ -19,10 +21,14 @@ export class QuestionEditComponent {
     constructor(private activatedRoute: ActivatedRoute,
         private router: Router,
         private http: HttpClient,
+        private fb: FormBuilder,
         @Inject('BASE_URL') private baseUrl: string) {
 
         // create an empty object from the Quiz interface
         this.question = <Question>{};
+
+        // initialize the form
+        this.createForm();
 
         var id = +this.activatedRoute.snapshot.params["id"];
 
@@ -44,10 +50,32 @@ export class QuestionEditComponent {
         }
     }
 
+    createForm() {
+        this.form = this.fb.group({
+            Text: ['', Validators.required]
+        });
+    }
+
+    updateForm() {
+        this.form.setValue({
+            Text: this.question.Text
+        });
+    }
+
     onSubmit(question: Question) {
+
+        // build a temporary quiz object from form values
+        var tempQuestion = <Question>{};
+        tempQuestion.Text = this.form.value.Text;
+        tempQuestion.QuizId = this.question.QuizId;
+
         var url = this.baseUrl + "api/question";
 
         if (this.editMode) {
+            // don't forget to set the tempQuiz Id,
+            // otherwise the EDIT would fail!
+            tempQuestion.Id = this.question.Id;
+
             this.http
                 .post<Question>(url, question)
                 .subscribe(res => {
@@ -69,5 +97,28 @@ export class QuestionEditComponent {
 
     onBack() {
         this.router.navigate(["quiz/edit", this.question.QuizId]);
+    }
+
+    // retrieve a FormControl
+    getFormControl(name: string) {
+        return this.form.get(name);
+    }
+
+    // returns TRUE if the FormControl is valid
+    isValid(name: string) {
+        var e = this.getFormControl(name);
+        return e && e.valid;
+    }
+
+    // returns TRUE if the FormControl has been changed
+    isChanged(name: string) {
+        var e = this.getFormControl(name);
+        return e && (e.dirty || e.touched);
+    }
+
+    // returns TRUE if the FormControl is invalid after user changes
+    hasError(name: string) {
+        var e = this.getFormControl(name);
+        return e && (e.dirty || e.touched) && !e.valid;
     }
 }
--- TestMakerFreeWebApp/ClientApp/app/components/quiz/quiz-edit.component.ts
+++ TestMakerFreeWebApp/ClientApp/app/components/quiz/quiz-edit.component.ts
@@ -103,4 +103,27 @@ export class QuizEditComponent {
     onBack() {
         this.router.navigate(["home"]);
     }
+
+    // retrieve a FormControl
+    getFormControl(name: string) {
+        return this.form.get(name);
+    }
+
+    // returns TRUE if the FormControl is valid
+    isValid(name: string) {
+        var e = this.getFormControl(name);
+        return e && e.valid;
+    }
+
+    // returns TRUE if the FormControl has been changed
+    isChanged(name: string) {
+        var e = this.getFormControl(name);
+        return e && (e.dirty || e.touched);
+    }
+
+    // returns TRUE if the FormControl is invalid after user changes
+    hasError(name: string) {
+        var e = this.getFormControl(name);
+        return e && (e.dirty || e.touched) && !e.valid;
+    }
 }
